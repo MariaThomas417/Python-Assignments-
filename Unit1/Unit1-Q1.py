d9c1e73505e323816fa2e730172fc844b4553e23
MariaThomas417 <marianna.thomas0712@gmail.com>
2026-02-03 22:36:21 +0530

    Add files via upload

---
+++ Unit1/Unit1-Q1.py
@@ -0,0 +1,13 @@
+
+l1 = [10, 20, 30, 40, 50]
+print("Original List:", l1)
+print("First element:", l1[0])
+print("Last element:", l1[-1])
+l1.append(60)      
+print("After adding 60:", l1)
+l1.remove(30)     
+print("After removing 30:", l1)
+l1.sort()
+print("Sorted List:", l1)
+l1.reverse()
+print("Reversed List:", l1)
